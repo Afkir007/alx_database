465ce07cb45f30b04955410bc8051e730f9a33ab
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2024-01-24 10:58:55 +0100

    add

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- lists all privileges of the MySQL users
+USER="user_0d_1";
+PASSWORD="password";
---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creating a user in sql
+CREATE USER 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost'="password";
