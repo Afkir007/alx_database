eaf725ba32cfc6db33abdc05bb808eaedabe0be0
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2024-01-26 22:48:19 +0100

    sql querries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privileges of the MySQL users
-USER="user_0d_1";
-PASSWORD="password";
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- creating a user in sql
-CREATE USER 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost'="password";
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
